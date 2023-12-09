40a936e299b15a73e231d771ddd9b9767a040dc7
sheraz <sherazarshad419@gmail.com>
2023-12-09 23:23:53 +0500

    faqs done

--- assets/script.js
+++ assets/script.js
@@ -10,6 +10,18 @@ document.addEventListener('DOMContentLoaded', function () {
 
     });
 
+    // SHOW MORE faqs
+    document.querySelector('.show-more-btn').addEventListener('click', (e) => {
+        const accordionWrapper = document.querySelector('.accordion-wrapper');
+        if (accordionWrapper.style.height === '100%') {
+            accordionWrapper.style.height = '900px';
+            document.querySelector('.show-more-btn').textContent = 'Show More >'
+        } else {
+            accordionWrapper.style.height = '100%';
+            document.querySelector('.show-more-btn').textContent = 'Show Less <'
+        }
+    });
+
     // faqs
     var acc = document.getElementsByClassName("accordion-heading");
     var i;
@@ -17,6 +29,9 @@ document.addEventListener('DOMContentLoaded', function () {
     for (i = 0; i < acc.length; i++) {
         acc[i].addEventListener("click", function () {
             this.classList.toggle("active");
+            this.querySelector('.plus').classList.toggle('hidden')
+            this.querySelector('.minus').classList.toggle('hidden')
+
             var panel = this.nextElementSibling;
             if (panel.style.maxHeight) {
                 panel.style.maxHeight = null;
